9879828cd595a7003b7c327ee4c24a1e5217d1ae
aslv24 <aslinfomats@gmail.com>
2025-04-25 13:12:59 +0530

    Java File created

---
+++ Sample.java
@@ -0,0 +1,22 @@
+public class Sample
+{
+
+
+public static int add(int a, int b)
+{
+int c;
+c=a+b;
+return c;
+}
+
+
+public static void main(String[] args)
+{
+
+int addValue=Sample.add(100,200);
+System.out.println(addValue);
+
+}
+
+
+}
